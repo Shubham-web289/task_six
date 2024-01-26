9dd29b29af9400361c9a80f9e87a742bf7ba2766
EC2 Default User <ec2-user@ip-172-16-85-63.ap-south-1.compute.internal>
2024-01-26 10:50:50 +0000

    an yet another attempt to see the git hub action

--- count_ipynb_file.py
+++ count_ipynb_file.py
@@ -2,6 +2,6 @@ def count_files():
     import os
     ls = os.listdir()
     ls_only_py_files = len(list(filter(lambda x : x.endswith(".py"),ls)))
-    return(ls_only_py_files)
+    print("number of python files is :",ls_only_py_files)
 
 count_files()
